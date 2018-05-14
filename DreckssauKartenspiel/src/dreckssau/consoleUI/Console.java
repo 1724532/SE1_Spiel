d6c6702092b037c640e704d5c38def433a43ab0d
Lukas Theisen <1724673@stud.hs-mannheim.de>
2018-05-14 10:30:44 +0200

    Interface implemented for Game Hanndling in UI

--- DreckssauKartenspiel/src/dreckssau/consoleUI/Console.java
+++ DreckssauKartenspiel/src/dreckssau/consoleUI/Console.java
@@ -1,27 +1,36 @@
 package dreckssau.consoleUI;
 
+import dreckssau.game.IDreckssauHandler;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 
 /**
- * The class is used for handling the UI experience and player input and action handling within the game.
+ * The class is used for handling the UI experience and player input and
+ * action handling within the game.
  *
  * @author Lukas Theisen <1724673@stud.hs-mannheim.de>
  */
 public class Console {
 
+    private IDreckssauHandler = new Game();
     private InputStreamReader systemIn = new InputStreamReader(System.in);
     private BufferedReader consoleIn = new BufferedReader(systemIn);
 
+    public static void main(String[] args) {
+        Console console = new Console();
+        console.startConsole();
+    }
 
     /**
-     * The method which handles the UI during gameplay and manages the user input and view presentation for the player.
+     * The method which handles the UI during gameplay and manages the user
+     * input and view presentation for the player.
      */
     private void startConsole() {
         this.welcomeMessage();
         this.getGameConfiguration();
+        int c = 22;
     }
 
     private void getGameConfiguration() {
@@ -35,7 +44,8 @@ public class Console {
             e.printStackTrace();
         }
 
-        if (inputNumber != null && !inputNumber.equals("") && inputNumber.matches("[2-4]")) {
+        if (inputNumber != null && !inputNumber.equals("") && inputNumber
+                .matches("[2-4]")) {
             amountBots = Integer.parseInt(inputNumber);
         } else {
             System.out.println(">> Input has to be a number between 2 - 4! Please enter a correct number!");
@@ -46,14 +56,27 @@ public class Console {
         int[] difficultiesBots = new int[amountBots];
 
         for (int i = 0; i < difficultiesBots.length; i++) {
-            System.out.println("> Difficulty of computer player " + (i + 1) + ":");
+            System.out.println("> Difficulty of computer player " + (i + 1) +
+                    ":");
             difficultiesBots[i] = this.getDifficultyBot();
         }
 
         System.out.println("\n> Before we start we need your player name!");
         String playerName = this.getName();
 
-        System.out.println("\n>>> Thank you, " + playerName + "! You entered all skill levels! Game is ready to begin! <<<");
+        System.out.println("\n>>> Thank you, " + playerName + "! You entered all skill levels! Game is ready to " +
+                "begin in... <<<");
+//        System.out.println("3...");
+//        try {
+//            Thread.sleep(1000);
+//            System.out.println("2...");
+//            Thread.sleep(1000);
+//            System.out.println("1...");
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
     }
 
     private int getDifficultyBot() {
@@ -65,7 +88,8 @@ public class Console {
             e.printStackTrace();
         }
 
-        if (inputNumber != null && !inputNumber.equals("") && inputNumber.matches("[1-3]")) {
+        if (inputNumber != null && !inputNumber.equals("") && inputNumber
+                .matches("[1-3]")) {
             return Integer.parseInt(inputNumber);
         } else {
             System.out.println("Invalid skill level input! Please reenter your desired skill level [1-3]:");
@@ -74,16 +98,19 @@ public class Console {
     }
 
     /**
-     * The method shows a Welcome Message to the player and informs about the creators and their origin.
+     * The method shows a Welcome Message to the player and informs about the
+     * creators and their origin.
      */
     private void welcomeMessage() {
         System.out.println("/ - - - - - - - - - - - - \\");
         System.out.println("| >> D R E C K S S A U << |");
         System.out.println("\\ - - - - - - - - - - - - /");
         System.out.println("----------------------------");
-        System.out.println("> programmed by Team 1 | SE1\n> Hochschule Mannheim | 2UIB");
+        System.out.println("> programmed by Team 1 | SE1\n> Hochschule " + "Mannheim | 2UIB");
         System.out.println("----------------------------");
-        System.out.println("You are able to play against 2 - 4 computer players for which you define the skill level!\n");
+        System.out.println("You are able to play against 2 - 4 computer " + "players for which you define the " +
+                "skill level!\n");
+
     }
 
     private String getName() {
@@ -98,19 +125,14 @@ public class Console {
             e.printStackTrace();
         }
 
-        if (inputName != null && !inputName.equals("") && inputName.length() <= 25)
+        if (inputName != null && !inputName.equals("") && inputName.length()
+                <= 25)
             return inputName;
 
         else {
             System.out.println("Your name is invalid for playing, please reenter a valid name again!");
             return this.getName();
         }
-    }
-
-
-    public static void main(String[] args) {
-        Console console = new Console();
-        console.startConsole();
     }
 
 
---
+++ DreckssauKartenspiel/src/dreckssau/game/IDreckssauHandler.java
@@ -0,0 +1,34 @@
+package dreckssau.game;
+
+public interface IDreckssauHandler {
+
+    public void startGame();
+
+    public boolean getGameIsRunning();
+
+    public void addAIPlayer(int difficulty);
+
+    public void addHumanPlayer(String name);
+
+    public String getAllPlayers();
+
+    public String getCurrentPlayer();
+
+    public String getGamePhase();
+
+    public String getHand();
+
+    public int getRound();
+
+    public void doNextStep(int i);
+
+    public void doNextStep();
+
+    public String getBidStatus();
+
+    public String getCardStatus();
+
+    public int getTricksToPlay();
+
+
+}
